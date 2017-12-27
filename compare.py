1491f3e7af2a9e97c750b36bae8fb64f69e25a1a
Petter Chr. Bjelland <petter.bjelland@gmail.com>
2017-12-27 21:41:48 +0100

    .

--- compare.py
+++ compare.py
@@ -7,6 +7,9 @@ with open('res-sd.txt') as inp:
 
 hits = {}
 miss = {}
+i = 0
+m = 0
+n = 0
 with open('res.txt') as inp:
     for line in inp:
         parts = line.split()
@@ -17,18 +20,23 @@ with open('res.txt') as inp:
         if (a, b) in cache or (b, a) in cache:
             if (a, b) in cache:
                 hits[(a,b)] = int(c)
-                #print cache[(a, b)]
+                print "<",  cache[(a, b)]
+                i+=1
                 del cache[(a, b)]
 
             if (b, a) in cache:
                 hits[(b,a)] = int(c)
-                #print cache[(b, a)]
+                print ">", cache[(b, a)]
+                i+=1
                 del cache[(b, a)]
 
         else:
-            print "!!", parts
-
+            m += 1
 
 for p, c in cache.items():
     if int(c) > 50:
-        print p, c
+        #print p, c
+        pass
+        n += 1
+
+print i,m ,n
